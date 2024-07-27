7a9097c17f0c2edabe9673a70d86642577af80e3
MikeKrueger75 <michael.krueger@europace.de>
2024-07-27 14:12:25 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    documents = [
+        "Grundbuch",
+        "Flurkarte",
+        "Teilungserklärung",
+        "Aufteilungsplan",
+        "Eintragungsbewilligung",
+        "Baulastenverzeichnis",
+        "Altlastenverzeichnis"
+    ]
+    return render_template('index.html', documents=documents)
+
+if __name__ == '__main__':
+    app.run(debug=True)
