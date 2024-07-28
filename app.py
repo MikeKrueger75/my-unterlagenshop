9d1e13330fb5a8120b326810f268d94cdfe2ab74
MikeKrueger75 <michael.krueger@europace.de>
2024-07-28 19:46:42 +0200

    add navbar and reorganize with base-template

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
-app.permanent_session_lifetime = timedelta(days=180)
+app.permanent_session_lifetime = timedelta(days=7)
 
 @app.route('/')
 def index():
@@ -20,11 +20,11 @@ def produkte():
         {"name": "Baulastenverzeichnis", "preis": 20, "beschreibung": "Das Baulastenverzeichnis enthält Informationen über öffentlich-rechtliche Verpflichtungen eines Grundstücks."},
         {"name": "Altlastenverzeichnis", "preis": 25, "beschreibung": "Das Altlastenverzeichnis gibt Auskunft über Boden- und Grundwasserverunreinigungen."}
     ]
-    return render_template('produkte.html', documents=documents)
+    return render_template('produkte.html', documents=documents, active_page='produkte')
 
 @app.route('/immobiliendaten')
 def immobiliendaten():
-    return render_template('immobiliendaten.html')
+    return render_template('immobiliendaten.html', active_page='immobiliendaten')
 
 @app.route('/kundendaten', methods=['GET', 'POST'])
 def kundendaten():
@@ -39,27 +39,27 @@ def kundendaten():
         session['plz'] = request.form['plz']
         session['ort'] = request.form['ort']
         return redirect(url_for('angaben'))
-    return render_template('kundendaten.html', session=session)
+    return render_template('kundendaten.html', session=session, active_page='kundendaten')
 
 @app.route('/angaben')
 def angaben():
-    return render_template('angaben.html')
+    return render_template('angaben.html', active_page='angaben')
 
 @app.route('/eigentuemervollmacht')
 def eigentuemervollmacht():
-    return render_template('eigentuemervollmacht.html')
+    return render_template('eigentuemervollmacht.html', active_page='eigentuemervollmacht')
 
 @app.route('/zusammenfassung')
 def zusammenfassung():
-    return render_template('zusammenfassung.html')
+    return render_template('zusammenfassung.html', active_page='zusammenfassung')
 
 @app.route('/bezahlung')
 def bezahlung():
-    return render_template('bezahlung.html')
+    return render_template('bezahlung.html', active_page='bezahlung')
 
 @app.route('/bestaetigung')
 def bestaetigung():
-    return render_template('bestaetigung.html')
+    return render_template('bestaetigung.html', active_page='bestaetigung')
 
 if __name__ == '__main__':
     app.run(debug=True)
